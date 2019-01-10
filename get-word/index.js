0a10bead97c1bd7b7b0699929d9fe160d8315cf5
huunghia98 <nguyenhuunghia295@gmail.com>
2019-01-10 14:54:12 +0700

    delete result if exist

--- get-word/index.js
+++ get-word/index.js
@@ -1,9 +1,16 @@
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 let fs = require("jsonfile");
+let ffs = require("fs");
+ffs.unlink("./result.json", function (error) {
+    if (error) {
+        throw error;
+    }
+    console.log('Deleted result.json!!');
+});
 let xhr = new XMLHttpRequest;
 let array;
 let url;
-for (let i = 0; i < 86800; i+=301) {
+for (let i = 0; i < 3000; i += 301) {
     url = 'http://www.wordcount.org/dbquery.php?toFind=' + i + '&method=SEARCH_BY_INDEX';
 //Call the open function, GET-type of request, url, true-asynchronous
     xhr.open('GET', url, false);
@@ -15,11 +22,11 @@ for (let i = 0; i < 86800; i+=301) {
         if (this.status === 200) {
             words = this.responseText;
         }
-        let count=i;
-        let inJson=[];
+        let count = i;
+        let inJson = [];
         console.log(count);
         array = words.split("&");
-        for (x = 4; x+1 < array.length; x += 2) {
+        for (x = 4; x + 1 < array.length; x += 2) {
             tmp = {
                 index: count++,
                 word: array[x].split("=")[1],
