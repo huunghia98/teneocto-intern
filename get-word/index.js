c2e235732583ea7a671fa052a44874de8575d44f
huunghia98 <nguyenhuunghia295@gmail.com>
2019-01-10 21:42:26 +0700

    last commit

--- get-word/index.js
+++ get-word/index.js
@@ -1,46 +1,39 @@
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 let fs = require("jsonfile");
-let ffs = require("fs");
-ffs.unlink("./result.json", function (error) {
-    if (error) {
-        throw error;
-    }
-    console.log('Deleted result.json!!');
-});
 let xhr = new XMLHttpRequest;
 let array;
 let url;
-for (let i = 0; i < 86800; i += 301) {
-    url = 'http://www.wordcount.org/dbquery.php?toFind=' + i + '&method=SEARCH_BY_INDEX';
-//Call the open function, GET-type of request, url, true-asynchronous
-    xhr.open('GET', url, false);
+let inJson=[];
+function get_word() {
+    for (let i = 0; i < 86800; i += 301) {
+        url = 'http://www.wordcount.org/dbquery.php?toFind=' + i + '&method=SEARCH_BY_INDEX';
+        xhr.open('GET', url, false);
 // //call the onload
 // xhr.setRequestHeader('Access-Control-Allow-Origin', "*");
 // xhr.setRequestHeader('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
 // xhr.setRequestHeader('Access-Control-Allow-Headers', 'Content-Type');
-    xhr.onload = function () {
-        if (this.status === 200) {
-            words = this.responseText;
-        }
-        let count = i;
-        let inJson = [];
-        console.log(count);
-        array = words.split("&");
-        for (x = 4; x + 1 < array.length; x += 2) {
-            tmp = {
-                index: count++,
-                word: array[x].split("=")[1],
-                freq: array[x + 1].split("=")[1]
+        xhr.onload = function () {
+            if (this.status === 200) {
+                words = this.responseText;
             }
-            inJson.push(tmp);
+            let count = i;
+            console.log(count);
+            array = words.split("&");
+            for (x = 4; x + 1 < array.length; x += 2) {
+                tmp = {
+                    index: ++count,
+                    word: array[x].split("=")[1],
+                    freq: array[x + 1].split("=")[1]
+                }
+                inJson.push(tmp);
+            }
         }
-        fs.writeFile('./result.json', JSON.stringify(inJson), {flag: 'a'}, err => {
-            if (err)
-                console.log(err);
-        });
+        xhr.send();
     }
-    xhr.send();
+    fs.writeFile('./result.json', JSON.stringify(inJson) , err => {
+        if (err)
+            console.log(err);
+    });
+    console.log(inJson);
 }
-
-
-
+get_word();
